522465b914415d738f38a88498c58532039a6812
lidorlg <lidorlg@gmail.com>
2015-09-20 17:27:48 +0300

    ver 1.2

--- Workspace/Test/src/HelloWorld.java
+++ Workspace/Test/src/HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.print("this is a test hello");
+	System.out.println("this is a test hello");
 	System.out.println("Wlcome to my world ");
 }
 }
