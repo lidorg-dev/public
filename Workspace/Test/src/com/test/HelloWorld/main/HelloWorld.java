0ddfd4cba210cc45a41b3667a9f2f468d5a1ac47
lidorlg <lidorlg@gmail.com>
2015-09-21 10:56:52 +0300

    ready

--- Workspace/Test/src/com/test/HelloWorld/main/HelloWorld.java
+++ Workspace/Test/src/com/test/HelloWorld/main/HelloWorld.java
@@ -3,6 +3,6 @@ package com.test.HelloWorld.main;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("this is a test hello");
-	System.out.println("Wlcome to my world ");
+	System.out.println("Welcome to my world ");
 }
 }
